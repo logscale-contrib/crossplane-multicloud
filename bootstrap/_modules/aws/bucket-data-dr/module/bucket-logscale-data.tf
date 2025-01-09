4226f4f63b418d9625780a6a199d29150f969ef9
Ryan Faircloth <ryan@dss-i.com>
2025-01-09 10:11:25 -0600

    feat(bootstrap): add data bucket

---
+++ bootstrap/_modules/aws/bucket-data-dr/module/bucket-logscale-data.tf
@@ -0,0 +1,39 @@
+
+module "data-dr" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.3.0"
+
+  bucket_prefix = var.name
+  # acl           = "private"
+
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerEnforced"
+
+  versioning = {
+    enabled = true
+  }
+
+  logging = {
+    target_bucket = var.logs_s3_bucket_id
+    target_prefix = "S3Logs/"
+  }
+
+  lifecycle_rule = [
+    {
+      id                                     = "storage"
+      enabled                                = true
+      abort_incomplete_multipart_upload_days = 7
+
+    }
+  ]
+  server_side_encryption_configuration = {
+    rule = {
+      "apply_server_side_encryption_by_default" = {
+        "kms_master_key_id" = ""
+        "sse_algorithm" : "AES256"
+      }
+      bucket_key_enabled = true
+    }
+  }
+
+}
---
+++ bootstrap/_modules/aws/bucket-data-dr/module/outputs.tf
@@ -0,0 +1,6 @@
+output "data-dr_bucket_arn" {
+  value = module.data-dr.s3_bucket_arn
+}
+output "data-dr_bucket_id" {
+  value = module.data-dr.s3_bucket_id
+}

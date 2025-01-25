38cc7b2a6ffa32500ebc6eee83bacb2ff7ba053e
Ryan Faircloth <ryan@dss-i.com>
2025-01-25 11:15:59 -0600

    Update authentik-instance.tf

--- bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
+++ bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
@@ -1,15 +1,17 @@
 
-# resource "kubectl_manifest" "authentik" {
-#     count = var.region_name
+resource "kubectl_manifest" "authentik" {
+  count = (
+    var.db_state[var.region_name]["name"] == "normal"
+  ) ? 1 : 0
 
-#   yaml_body = templatefile("./manifests/helm-releases/authentik.yaml",
-#     {
-#     smtp_user     = var.smtp_user
-#     smtp_password = var.smtp_password
-#     smtp_server   = var.smtp_server
-#     smtp_port     = var.smtp_port
-#     smtp_tls      = "${var.smtp_tls}"
-#     from_email    = var.from_email
-#   })
+  yaml_body = templatefile("./manifests/helm-releases/authentik.yaml",
+    {
+      smtp_user     = var.smtp_user
+      smtp_password = var.smtp_password
+      smtp_server   = var.smtp_server
+      smtp_port     = var.smtp_port
+      smtp_tls      = "${var.smtp_tls}"
+      from_email    = var.from_email
+  })
 
-# }
+}

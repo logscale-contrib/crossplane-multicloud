38dffd300e78413fb25d5eee7f3ccb40f990ec44
Ryan Faircloth <ryan@dss-i.com>
2025-01-25 11:19:23 -0600

    Update authentik-instance.tf

--- bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
+++ bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
@@ -6,8 +6,8 @@ resource "kubectl_manifest" "authentik" {
 
   yaml_body = templatefile("./manifests/helm-releases/authentik.yaml",
     {
-      smtp_user     = var.smtp_user
-      smtp_password = var.smtp_password
+      smtp_user     = module.iam_ses_user.iam_access_key_id
+      smtp_password = module.iam_ses_user.iam_access_key_ses_smtp_password_v4
       smtp_server   = var.smtp_server
       smtp_port     = var.smtp_port
       smtp_tls      = "${var.smtp_tls}"

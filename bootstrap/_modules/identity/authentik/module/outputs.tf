94f3f1217969ede8b3ceb798b7127acfe271288d
Ryan Faircloth <ryan@dss-i.com>
2025-01-26 07:46:18 -0600

    Update outputs.tf

--- bootstrap/_modules/identity/authentik/module/outputs.tf
+++ bootstrap/_modules/identity/authentik/module/outputs.tf
@@ -1,7 +1,7 @@
 
 output "authentik_cookie_key_policy_arn" {
-  value = module.secrets_manager.policy_arn  
+  value = module.authentik_cookie_key.policy_arn  
 }
 output "authentik_cookie_key_ssm_name" {
-  value = module.secrets_manager.secret_name    
+  value = module.authentik_cookie_key.secret_name    
 }

dba9242a597c1898be368629fb9aea32e60fcd90
Ryan Faircloth <ryan@dss-i.com>
2025-01-25 18:25:45 -0600

    Update authentik-instance.tf

--- bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
+++ bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
@@ -1,3 +1,61 @@
+module "authentik_server" {
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.52.2"
+
+  role_name_prefix = "${var.authentik_namespace}-server"
+  role_path        = var.iam_role_path
+
+#   role_policy_arns = {
+#     "object" = module.iam_iam-policy.arn
+#   }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["${var.authentik_namespace}:authentik-server"]
+    }
+  }
+}
+
+module "authentik_worker" {
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.52.2"
+
+  role_name_prefix = "${var.authentik_namespace}-woker"
+  role_path        = var.iam_role_path
+
+#   role_policy_arns = {
+#     "object" = module.iam_iam-policy.arn
+#   }
+
+  oidc_providers = {
+    main = {
+      provider_arn               = var.oidc_provider_arn
+      namespace_service_accounts = ["${var.authentik_namespace}:authentik-worker"]
+    }
+  }
+}
+
+resource "kubernetes_service_account" "authentik_server" {
+  metadata {
+    name = "authentik-server"
+    annotations = {
+      "iam.amazonaws.com/role" = module.authentik_server.role_name
+    }
+  }
+}
+
+resource "kubernetes_service_account" "authentik_worker" {
+  metadata {
+    name = "authentik-worker"
+    annotations = {
+      "iam.amazonaws.com/role" = module.authentik_worker.role_name
+    }
+  }
+}
+
 
 resource "kubectl_manifest" "authentik_instance" {
   count = (

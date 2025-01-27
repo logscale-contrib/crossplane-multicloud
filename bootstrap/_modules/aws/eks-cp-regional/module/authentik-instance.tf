5cb6f30968b17471769d4df1e0479a3692ececf9
Ryan Faircloth <ryan@dss-i.com>
2025-01-27 14:11:13 -0600

    db setup

--- bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
+++ bootstrap/_modules/aws/eks-cp-regional/module/authentik-instance.tf
@@ -64,16 +64,17 @@ resource "kubectl_manifest" "authentik_instance" {
 
   yaml_body = templatefile("./manifests/helm-releases/authentik.yaml",
     {
-      namespace     = var.authentik_namespace
-      smtp_user     = module.iam_ses_user.iam_access_key_id
-      smtp_password = module.iam_ses_user.iam_access_key_ses_smtp_password_v4
-      smtp_server   = var.smtp_server
-      smtp_port     = var.smtp_port
-      smtp_tls      = "${var.smtp_tls}"
-      from_email    = var.from_email
+      region_name                   = var.region_name
+      namespace                     = var.authentik_namespace
+      smtp_user                     = module.iam_ses_user.iam_access_key_id
+      smtp_password                 = module.iam_ses_user.iam_access_key_ses_smtp_password_v4
+      smtp_server                   = var.smtp_server
+      smtp_port                     = var.smtp_port
+      smtp_tls                      = "${var.smtp_tls}"
+      from_email                    = var.from_email
       authentik_cookie_key_ssm_name = var.authentik_cookie_key_ssm_name
-      sa_server_arn = module.authentik_server.iam_role_arn
-      sa_worker_arn = module.authentik_worker.iam_role_arn
+      sa_server_arn                 = module.authentik_server.iam_role_arn
+      sa_worker_arn                 = module.authentik_worker.iam_role_arn
   })
 
 }

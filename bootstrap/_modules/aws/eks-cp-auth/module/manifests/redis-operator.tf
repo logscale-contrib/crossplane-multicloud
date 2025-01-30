eb4fba08ad8991a3810fff1ad930b1ed6f3d7e6d
Ryan Faircloth <ryan@dss-i.com>
2025-01-29 20:21:12 -0600

    Update redis-operator.tf

--- bootstrap/_modules/aws/eks-cp-auth/module/manifests/redis-operator.tf
+++ bootstrap/_modules/aws/eks-cp-auth/module/manifests/redis-operator.tf
@@ -3,5 +3,5 @@ resource "kubectl_manifest" "redis-operator" {
   depends_on = [
     time_sleep.external_services
   ]
-  yaml_body = templatefile("./manifests/helm-manifests/redis-operator.yaml", {})
+  yaml_body = templatefile("./manifests/helm-releases/redis-operator.yaml", {})
 }

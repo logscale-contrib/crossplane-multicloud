86da14611a188f2f63311b2ed5db9bb486572f06
Ryan Faircloth <ryan@dss-i.com>
2025-01-29 20:59:52 -0600

    Update versions.tf

--- bootstrap/_modules/identity/authentik-partition/module/versions.tf
+++ bootstrap/_modules/identity/authentik-partition/module/versions.tf
@@ -35,7 +35,7 @@ terraform {
     }
     checkmate = {
       source  = "tetratelabs/checkmate"
-      version = "2.0.0"
+      version = "1.8.4"
     }
     random = {
       source  = "hashicorp/random"

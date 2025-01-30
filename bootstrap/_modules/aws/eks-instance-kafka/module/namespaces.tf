c3acaed619500f8b674a3d14160ca9c3834f91be
Ryan Faircloth <ryan@dss-i.com>
2025-01-30 11:52:47 -0600

    fix

--- bootstrap/_modules/aws/eks-instance-kafka/module/namespaces.tf
+++ bootstrap/_modules/aws/eks-instance-kafka/module/namespaces.tf
@@ -1,9 +1,12 @@
 
 data "kubectl_path_documents" "namespaces" {
   pattern = "./manifests/namespaces/*.yaml"
+  vars = {
+    namespace = var.namespace
+  }
 }
 
 resource "kubectl_manifest" "namespaces" {
   for_each  = data.kubectl_path_documents.namespaces.manifests
-  yaml_body = templatefile(each.value, { namespace = var.namespace })
+  yaml_body = each.value
 }

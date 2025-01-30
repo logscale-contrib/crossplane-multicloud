1b6f2bad53cf01f4059b09acd75ad74bc11550a4
Ryan Faircloth <ryan@dss-i.com>
2025-01-30 11:58:07 -0600

    Update redis-operator.tf

--- bootstrap/_modules/aws/eks-instance-kafka/module/manifests/redis-operator.tf
+++ bootstrap/_modules/aws/eks-instance-kafka/module/manifests/redis-operator.tf
@@ -1,7 +1,5 @@
 
 resource "kubectl_manifest" "redis-operator" {
-  depends_on = [
-    time_sleep.external_services
-  ]
+
   yaml_body = templatefile("./manifests/helm-releases/redis-operator.yaml", {})
 }

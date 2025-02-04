af8cf226c8b591204bf10639748cbc17aa8eeeb4
Ryan Faircloth <ryan@dss-i.com>
2025-02-04 15:47:46 -0700

    Update main.tf

--- bootstrap/_modules/aws/eks-instance-logscale/module/main.tf
+++ bootstrap/_modules/aws/eks-instance-logscale/module/main.tf
@@ -1,19 +1,19 @@
 
 
 
-data "kubectl_path_documents" "flux2-releases" {
-  pattern = "./manifests/helm-releases/*.yaml"
-  vars = {
-    kafka_namespace    = var.kafka_namespace
-    cluster_name       = var.cluster_name
-    kafka_name         = var.kafka_name
-    logscale_namespace = var.logscale_namespace
-  }
-}
+# data "kubectl_path_documents" "flux2-releases" {
+#   pattern = "./manifests/helm-releases/*.yaml"
+#   vars = {
+#     kafka_namespace    = var.kafka_namespace
+#     cluster_name       = var.cluster_name
+#     kafka_name         = var.kafka_name
+#     logscale_namespace = var.logscale_namespace
+#   }
+# }
 
-resource "kubectl_manifest" "flux2-releases" {
-  depends_on = [kubectl_manifest.namespaces]
+# resource "kubectl_manifest" "flux2-releases" {
+#   depends_on = [kubectl_manifest.namespaces]
 
-  for_each  = data.kubectl_path_documents.flux2-releases.manifests
-  yaml_body = each.value
-}
+#   for_each  = data.kubectl_path_documents.flux2-releases.manifests
+#   yaml_body = each.value
+# }

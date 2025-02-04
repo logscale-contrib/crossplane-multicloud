458c2e6b6213e63fa159912da252d55e02110fa8
Ryan Faircloth <ryan@dss-i.com>
2025-02-04 12:03:27 -0700

    Update variables.tf

--- bootstrap/_modules/aws/eks-instance-logscale/module/variables.tf
+++ bootstrap/_modules/aws/eks-instance-logscale/module/variables.tf
@@ -1,7 +1,4 @@
-variable "namespace" {
-  type        = string
-  description = "(optional) describe your variable"
-}
+
 variable "cluster_name" {
   type        = string
   description = "(optional) describe your variable"

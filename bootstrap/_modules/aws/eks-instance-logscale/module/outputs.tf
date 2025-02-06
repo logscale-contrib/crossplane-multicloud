77c28ddd93bf5831a56d314bf29ef61c2990c210
Ryan Faircloth <ryan@dss-i.com>
2025-02-06 13:36:36 -0700

    Update outputs.tf

--- bootstrap/_modules/aws/eks-instance-logscale/module/outputs.tf
+++ bootstrap/_modules/aws/eks-instance-logscale/module/outputs.tf
@@ -4,3 +4,11 @@ output "logscale_bucket_prefix_base" {
 output "logscale_account_arn" {
   value = module.logscale_service_account.iam_role_arn
 }
+output "logscale_account" {
+  value = var.var.logscale_namespace
+}
+output "logscale_account_annotations" {
+  value = {
+    "eks.amazonaws.com/role-arn" = module.logscale_service_account.iam_role_arn
+  }
+}

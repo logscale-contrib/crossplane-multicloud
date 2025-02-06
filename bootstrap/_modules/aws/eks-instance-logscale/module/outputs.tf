3eb7912c989180ccce64252ac9ea7d1955223d7e
Ryan Faircloth <ryan@dss-i.com>
2025-02-06 13:34:55 -0700

    Update outputs.tf

--- bootstrap/_modules/aws/eks-instance-logscale/module/outputs.tf
+++ bootstrap/_modules/aws/eks-instance-logscale/module/outputs.tf
@@ -2,5 +2,5 @@ output "logscale_bucket_prefix_base" {
   value = local.bucket_prefix
 }
 output "logscale_account_arn" {
-  value = module.logscale_service_account.arn
+  value = module.logscale_service_account.iam_role_arn
 }

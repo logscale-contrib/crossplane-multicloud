5107b681fefae0b01ddc85540cb4959d84fe4dba
Ryan Faircloth <ryan@dss-i.com>
2025-02-16 10:01:34 -0700

    Update policy.tf

--- bootstrap/_modules/identity/authentik-sso-saml/module/policy.tf
+++ bootstrap/_modules/identity/authentik-sso-saml/module/policy.tf
@@ -1,5 +1,5 @@
-resource "authentik_policy_binding" "app-access" {
-  target = authentik_application.name.uuid
-  group  = authentik_group.users.id
-  order  = 0
-}
+# resource "authentik_policy_binding" "app-access" {
+#   target = authentik_application.name.uuid
+#   group  = authentik_group.users.id
+#   order  = 0
+# }

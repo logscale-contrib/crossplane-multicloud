cd358fce7f0fa53c24ee0a3d82be512eae868964
Ryan Faircloth <ryan@dss-i.com>
2025-02-17 08:05:39 -0700

    Update cert.tf

--- bootstrap/_modules/identity/authentik-sso-saml/module/cert.tf
+++ bootstrap/_modules/identity/authentik-sso-saml/module/cert.tf
@@ -1,6 +1,8 @@
 resource "tls_private_key" "kp" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P384"
+  #   algorithm   = "ECDSA"
+  #   ecdsa_curve = "P384"
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
 resource "tls_self_signed_cert" "cert" {
@@ -20,9 +22,17 @@ resource "tls_self_signed_cert" "cert" {
     # "server_auth",
   ]
 }
+resource "random_string" "crt-name" {
+  length           = 4
+  special          = false
+  numeric          = false
+  upper            = false
+  override_special = "/@£$"
+  keepers          = { cert = tls_self_signed_cert.cert.cert_pem }
+}
 
 resource "authentik_certificate_key_pair" "saml" {
-  name             = "${var.tenantName}-${var.appName} Self-signed SAML Certificate"
+  name             = "${var.tenantName}-${var.appName} Self-signed SAML Certificate ${random_string.crt-name.result}"
   certificate_data = tls_self_signed_cert.cert.cert_pem
   key_data         = tls_private_key.kp.private_key_pem
 }
